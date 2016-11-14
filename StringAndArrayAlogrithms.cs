fe15b326a0aca25a06a11af6ab24f99a8a7db215
David Saelman <Daves5771@gmail.com>
2016-11-14 13:58:37 +0200

    Improvements and added a few more functions

--- StringAndArrayAlogrithms.cs
+++ StringAndArrayAlogrithms.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Fast_Algorithms
 {
-    class StringAlogrithms
+    class StringAndArrayAlogrithms
     {
-        const int NUMBER_CHARS = 256;
+        const int NUMBER_ASCII_CHARS = 256;
         const int NUMBER_ASCII_NUMERALS = 10;
         const int FIRST_ASCII_NUMBER = 48;
 
@@ -17,10 +17,17 @@ namespace Fast_Algorithms
             bool result = IsAnagram("my first test", "first my test");
             result = IsAnagram("bbaaccddeeffgg", "bbaaccddeeffhh");
 
-            string outputStr = "";
-            result = ReturnUniqueSymbols("bbaaccddeeffgg", ref outputStr);
+            StringBuilder outputStr = new StringBuilder(); 
+            result = ReturnUniqueSymbols("bbaaccddeeffgg",  ref outputStr);
             int[] inputArray = new int[] { 2, 2,7,6,5,4,8,4,4 };
+
+            outputStr.Clear();
             result = ReturnUniqueNumericalSymbols(inputArray, ref outputStr);
+            int length = 0;
+            result = ReturnUniqueNumericalSymbols(inputArray, ref length);
+            string in_str = "bbagaccdgdeeffgg";
+            char maxChar = '0';
+            int num = ReturnMaxCharOccurance(in_str, ref maxChar);
         }
 
         // O(nlogn) - O[1] memory space - sort both arrays and compare if strings are equal
@@ -34,8 +41,8 @@ namespace Fast_Algorithms
         static bool IsAnagram(string str1, string str2)
         {
             // assuming we only have ascii characters and we are limiting the number of any one character to 2^32-1
-            int[] lookUp1 = new int[NUMBER_CHARS];
-            int[] lookUp2 = new int[NUMBER_CHARS];
+            int[] lookUp1 = new int[NUMBER_ASCII_CHARS];
+            int[] lookUp2 = new int[NUMBER_ASCII_CHARS];
 
             int len1 = str1.Length;
             int len2 = str2.Length;
@@ -52,7 +59,7 @@ namespace Fast_Algorithms
             }
 
             // if the strings are anagrams than the char counts are equal
-            for (int i = 0; i < NUMBER_CHARS; i++)
+            for (int i = 0; i < NUMBER_ASCII_CHARS; i++)
             {
                 if (lookUp1[i] != lookUp2[i])
                     return false;
@@ -64,13 +71,16 @@ namespace Fast_Algorithms
 
         // in c# JAVA we can use a StringBuilder data structure, for C++ we use an unordered_set
         // O[n] function rountine
-        static bool ReturnUniqueSymbols(string in_str, ref string output)
+        static bool ReturnUniqueSymbols(string in_str, ref StringBuilder outputStr)
         {
             // assuming we only have ascii characters and we are limiting the number of any one character to 2^32-1
             // if we assume only ascii numeric cahracters, we can limit the size of our lookup tables to 4* 10 40 Bytes!
-            int[] lookUp1 = new int[NUMBER_CHARS];
-            int[] lookUp2 = new int[NUMBER_CHARS];
-            StringBuilder outputStr = new StringBuilder(in_str.Length);
+            int[] lookUp1 = new int[NUMBER_ASCII_CHARS];
+            int[] lookUp2 = new int[NUMBER_ASCII_CHARS];
+
+            // we want to minimize the amount of times we resize
+            // NUMBER_ASCII_CHARS is an upperbounds for the capacity
+            outputStr = new StringBuilder(NUMBER_ASCII_CHARS);
 
             // we peroform one pass of the string O[n]
             for (int i = 0; i < in_str.Length; i++)
@@ -88,17 +98,18 @@ namespace Fast_Algorithms
                 return false;
 
             // this copy is O[n] but we do it once at the end
-            output = outputStr.ToString();
             return true;
         }
 
         // in c# JAVA we can use a StringBuilder data structure, for C++ we use an unordered_set
-        static bool ReturnUniqueNumericalSymbols(int[] in_str, ref string output)
+        static bool ReturnUniqueNumericalSymbols(int[] in_str, ref StringBuilder outputStr)
         {
             // assuming we only have ascii characters and we are limiting the number of any one character to 2^32-1
             int[] lookUp1 = new int[NUMBER_ASCII_NUMERALS];
-            int[] lookUp2 = new int[NUMBER_ASCII_NUMERALS];
-            StringBuilder outputStr = new StringBuilder(in_str.Length);
+
+            // we want to minimize the amount of times we resize
+            // NUMBER_ASCII_NUMERALS is an upperbounds for the capacity
+            outputStr = new StringBuilder(in_str.Length);
 
             // we peroform one pass of the string O[n]
             for (int i = 0; i < in_str.Length; i++)
@@ -116,8 +127,63 @@ namespace Fast_Algorithms
                 return false;
 
             // this copy is O[n] but we do it once at the end
-            output = outputStr.ToString();
             return true;
+        }
+
+        // this algorithm does not use an output StringBuilder
+        // if we can overwrite the orignal input array
+        static bool ReturnUniqueNumericalSymbols(int[] in_str, ref int outputLength)
+        {
+            // assuming we only have ascii characters and we are limiting the number of any one character to 2^32-1
+            int[] lookUp1 = new int[NUMBER_ASCII_NUMERALS];
+            outputLength = 0;
+
+            // we peroform one pass of the string O[n]
+            for (int i = 0; i < in_str.Length; i++)
+            {
+                if (lookUp1[in_str[i]] == 0) // first occurance of the symbol
+                {
+                    // add the char to the output
+                    in_str[outputLength++] = in_str[i];
+                    // don't use this character again
+                    ++(lookUp1[(int)in_str[i]]);
+                }
+            }
+
+            if (outputLength == 0)
+                return false;
+
+            // this copy is O[n] but we do it once at the end
+            return true;
+        }
+
+        // returns the character which occurs the maximum times
+        // returns the number of times it appears
+        // O[n] - 1 scan to build lookup table, 1 scan to find max occurance
+        static int ReturnMaxCharOccurance(string in_str, ref char maxChar)
+        {
+            int length = in_str.Length;
+            if (length == 0)
+                return 0;
+
+            int[] lookUp1 = new int[NUMBER_ASCII_CHARS];
+
+            // build lookup table
+            for (int i = 0; i < length; i++)
+                lookUp1[in_str[i]]++;
+
+            // find character with max occurance
+            int max = 1;
+
+            for (int i = 0; i < NUMBER_ASCII_CHARS; i++)
+            {
+                if (lookUp1[i] > max)
+                {
+                    max = lookUp1[i];
+                    maxChar = (char)i;
+                }
+            }
+            return max;
         }
     }
  }
